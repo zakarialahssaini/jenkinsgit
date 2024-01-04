ad3d3d5c5b7c76b251e2d34b3c17ead0fc14923f
zakarialahssaini <lhziko7@gmail.com>
2024-01-04 13:39:52 +0100

    modificatins

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -1,11 +1,21 @@
 package StepDefinitions;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",glue= {"StepDefinitions"})
+@CucumberOptions(
+		features="src/test/resources/Features",
+		glue= {"StepDefinitions"},
+        tags = "@search",
+    		plugin = {
+    			    "pretty",
+    			    "html:target/cucumber-html-report", 
+    			    "json:target/cucumber/report.json",
+    			    "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    			},
+    dryRun = false, monochrome = true
+)
 public class TestRunner {
-	
 }
